473b1075c8f4c9dbd0360c3d205c0ff31d4e1567
praivesi <praivesi@gmail.com>
2026-02-07 13:00:24 +0900

    deploy: Configure for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { DataProvider } from './context/DataContext';
 import MainPage from './pages/MainPage';
 import ArtistTemplate from './pages/ArtistTemplate';
